246a1128ef409d6d4ad5fb8f60d0ba455280edb7
Peter Warren <peter.warren@csiro.au>
2016-12-09 10:01:24 +1100

    fixed performance issue with the use of temporary tables in SQL Server getdomaindata procedure

--- sql/SQLServer/createNVCLDBObjects.sql
+++ sql/SQLServer/createNVCLDBObjects.sql
@@ -1187,44 +1187,35 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE FUNCTION [dbo].[GETDOMAINDATAFUNC] 
-(
-	@v_domainlog_id varchar(max)
-)
-RETURNS 
-@Temp TABLE
-       (samplenumber   integer,
-        startvalue     numeric(18,5),
-        endvalue       numeric(18,5),
-        samplename     varchar(4000))
-AS
-BEGIN
-	 DECLARE
-         @subdomid varchar(64)
-
-      SELECT @subdomid = DOMAINLOGS.ISSUBDOMAINOFLOG_ID
-      FROM dbo.DOMAINLOGS
-      WHERE DOMAINLOGS.LOG_ID = @v_domainlog_id
-
-      IF (@subdomid IS NULL)
-        insert into @Temp SELECT samplenumber,startvalue,endvalue,samplename FROM DOMAINLOGDATA WHERE LOG_ID=@v_domainlog_id ORDER BY samplenumber
-      ELSE 
-         insert into @Temp SELECT subdom.samplenumber,min(maindom.startvalue) as startvalue,max(maindom.endvalue) as endvalue,subdom.samplename FROM dbo.GETDOMAINDATAFUNC(@subdomid) maindom inner join domainlogdata subdom on maindom.samplenumber BETWEEN subdom.startvalue ANd subdom.endvalue WHERE subdom.log_id=@v_domainlog_id Group BY subdom.samplenumber,subdom.samplename ORDER by subdom.samplenumber
-	
-	RETURN 
-END
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE PROCEDURE [dbo].[GETDOMAINDATA]
    @v_domainlog_id varchar(max)
 AS 
    
    BEGIN
-       SELECT * FROM dbo.GETDOMAINDATAFUNC(@v_domainlog_id)
+   	 DECLARE
+         @subdomid varchar(64), @subsubdomid varchar (64)
+
+	  SET NOCOUNT ON;
+
+      SELECT @subdomid = DOMAINLOGS.ISSUBDOMAINOFLOG_ID
+      FROM dbo.DOMAINLOGS
+      WHERE DOMAINLOGS.LOG_ID = @v_domainlog_id;
+
+	   SELECT @subsubdomid = DOMAINLOGS.ISSUBDOMAINOFLOG_ID
+      FROM dbo.DOMAINLOGS
+      WHERE DOMAINLOGS.LOG_ID = @subdomid;
+
+	  if (@subsubdomid is not null) RAISERROR ( 'Domains cannot have more than 1 level of sub domaining.', 18,1);
+	  ELSE 
+		  BEGIN
+		  IF (@subdomid IS NULL)
+			SELECT samplenumber,startvalue,endvalue,samplename FROM DOMAINLOGDATA WHERE LOG_ID=@v_domainlog_id ORDER BY samplenumber
+		  ELSE 
+			SELECT subdom.samplenumber,min(maindom.startvalue) as startvalue,max(maindom.endvalue) as endvalue,subdom.samplename FROM (SELECT samplenumber,startvalue,endvalue,samplename FROM DOMAINLOGDATA WHERE LOG_ID=@subdomid) maindom inner join domainlogdata subdom on maindom.samplenumber BETWEEN subdom.startvalue ANd subdom.endvalue WHERE subdom.log_id=@v_domainlog_id Group BY subdom.samplenumber,subdom.samplename ORDER by subdom.samplenumber
+	  END
+
    END
+
 GO
 SET ANSI_NULLS ON
 GO
