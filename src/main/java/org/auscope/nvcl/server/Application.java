e91139ace87278c0f85bb138117958c210c6099f
PeterWarren <peter.warren@csiro.au>
2020-02-07 17:00:31 +1100

    added CORS headers

--- src/main/java/org/auscope/nvcl/server/Application.java
+++ src/main/java/org/auscope/nvcl/server/Application.java
@@ -31,6 +31,8 @@ import org.springframework.jms.listener.adapter.MessageListenerAdapter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Configuration
@@ -53,6 +55,16 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 
     @Bean
     public NVCLDataSvcDao nvclDataSvcDao() {
