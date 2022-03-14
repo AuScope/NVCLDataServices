a5e3c51d9b96c506e2e90487313415e0a235de54
Peter Warren <peter.warren@csiro.au>
2022-03-14 17:44:13 +1100

    added missing RequestMethod import
    added missing RequestMethod import

--- src/main/java/org/auscope/nvcl/server/web/controllers/MenuController.java
+++ src/main/java/org/auscope/nvcl/server/web/controllers/MenuController.java
@@ -92,6 +92,7 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
